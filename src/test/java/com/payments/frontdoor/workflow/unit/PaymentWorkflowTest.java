678890c00cc9118ca0acd5aaa2c616c54c4c6003
liamford <liam.ford.au@gmail.com>
2025-01-30 17:06:46 +1100

    completed positive Unit test for payment workflow

--- src/test/java/com/payments/frontdoor/workflow/unit/PaymentWorkflowTest.java
+++ src/test/java/com/payments/frontdoor/workflow/unit/PaymentWorkflowTest.java
@@ -2,14 +2,19 @@ package com.payments.frontdoor.workflow.unit;
 
 import com.payments.frontdoor.activities.PaymentActivityImpl;
 import com.payments.frontdoor.activities.PaymentStepStatus;
+import com.payments.frontdoor.model.PaymentAuthorizationResponse;
 import com.payments.frontdoor.model.PaymentDetails;
+import com.payments.frontdoor.model.PaymentInstruction;
+import com.payments.frontdoor.model.PaymentOrderResponse;
 import com.payments.frontdoor.service.PaymentApiConnector;
 import com.payments.frontdoor.service.PaymentDispatcherService;
 import com.payments.frontdoor.swagger.model.Account;
+import com.payments.frontdoor.swagger.model.PaymentResponse;
 import com.payments.frontdoor.workflows.PaymentWorkflow;
 import com.payments.frontdoor.workflows.PaymentWorkflowImpl;
 import com.payments.frontdoor.workflows.RefundWorkflowImpl;
 import com.payments.frontdoor.workflows.ReportWorkflowImpl;
+import io.temporal.client.ActivityCompletionClient;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowOptions;
 import io.temporal.testing.TestWorkflowEnvironment;
@@ -20,27 +25,31 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @Timeout(value = 30, unit = TimeUnit.SECONDS)
 @RunWith(MockitoJUnitRunner.class)
 class PaymentWorkflowTest {
 
-    PaymentDispatcherService paymentDispatcherService = Mockito.mock(PaymentDispatcherService.class);
-    PaymentApiConnector paymentApiConnector = Mockito.mock(PaymentApiConnector.class);
-
+    private static final Logger log = LoggerFactory.getLogger(PaymentWorkflowTest.class);
+    private final PaymentDispatcherService paymentDispatcherService = mock(PaymentDispatcherService.class);
+    private final PaymentApiConnector paymentApiConnector = mock(PaymentApiConnector.class);
 
     @RegisterExtension
-    public  final TestWorkflowExtension testWorkflow =
+    public final TestWorkflowExtension testWorkflow =
             TestWorkflowExtension.newBuilder()
                     .setWorkflowTypes(PaymentWorkflowImpl.class, RefundWorkflowImpl.class, ReportWorkflowImpl.class)
                     .setActivityImplementations(new PaymentActivityImpl(paymentApiConnector, paymentDispatcherService))
@@ -49,12 +58,24 @@ class PaymentWorkflowTest {
 
     @Test
     @WorkflowInitialTime("2020-01-01T01:00:00Z")
-    void testProcessPayment(
+    void testPositiveProcessPayment(
             TestWorkflowEnvironment testEnv,
             WorkflowClient workflowClient,
             WorkflowOptions workflowOptions,
             Worker worker,
             PaymentWorkflow workflow) {
+
+        when(paymentApiConnector.callOrderPayment(any(PaymentInstruction.class)))
+                .thenReturn(PaymentOrderResponse.builder().status("completed").build());
+        when(paymentApiConnector.callAuthorizePayment(any(PaymentInstruction.class)))
+                .thenReturn(PaymentAuthorizationResponse.builder().status("success").build());
+
+        doAnswer(invocation -> {
+            byte[] receivedToken = invocation.getArgument(2);
+            ActivityCompletionClient completionClient = workflowClient.newActivityCompletionClient();
+            completionClient.complete(receivedToken, PaymentStepStatus.POSTED);
+            return null;
+        }).when(paymentDispatcherService).dispatchPayment(any(), eq(PaymentStepStatus.POSTED), any());
 
         Account debtor = new Account();
         debtor.setAccountNumber("123456789");
@@ -71,26 +92,29 @@ class PaymentWorkflowTest {
                 .debtor(debtor)
                 .creditor(creditor)
                 .paymentDate(LocalDate.now())
+                .headers(Map.of(
+                        "CORRELATION_ID", "123e4567-e89b-12d3-a456-426614174000",
+                        "x-idempotency-key", "TRANS-12345",
+                        "Content-Type", "application/json",
+                        "x-request-status", "201"
+                ))
                 .build();
+
+        WorkflowClient.start(workflow::processPayment, paymentDetails);
+        workflow.waitForStep(PaymentStepStatus.EXECUTED);
+        PaymentResponse response = workflow.processPayment(paymentDetails);
 
         assertAll(
                 () -> assertTrue(testEnv.isStarted()),
                 () -> assertNotNull(workflowClient),
                 () -> assertNotNull(workflowOptions.getTaskQueue()),
                 () -> assertNotNull(worker),
+                () -> assertNotNull(response),
+                () -> assertEquals(PaymentResponse.StatusEnum.ACSC, response.getStatus()),
+                () -> assertNotNull(workflow.getCompletedSteps()),
                 () -> assertEquals(
                         Instant.parse("2020-01-01T01:00:00Z"),
-                        Instant.ofEpochMilli(testEnv.currentTimeMillis()).truncatedTo(ChronoUnit.HOURS)),
-                () -> {
-                    try {
-                         workflow.processPayment(paymentDetails);
-                         workflow.waitForStep(PaymentStepStatus.EXECUTED);
-                    } catch (Exception e) {
-                        System.out.println("TIMEOUT ISSUE");
-                    }
-
-                    assertNotNull(workflow.getCompletedSteps());
-
-                });
+                        Instant.ofEpochMilli(testEnv.currentTimeMillis()).truncatedTo(ChronoUnit.HOURS))
+        );
     }
 }
